d651d5717a8941609fc05fda85af5ee1cc7924cb
Tracy Abrahms <tracyabrahms@gmail.com>
2013-03-11 16:08:47 -0700

    inital commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Learn a Module, Node.JS
+
+
+A command line tool that when called, will generate a random module.  
+	Built as a tool to expose myself to a module per day and familiarizing myself with great things happening in Node.JS.
+
+
+## LICENSE 
+
+MIT
---
+++ motd.js
@@ -0,0 +1,33 @@
+// Module of the Day randomizer
+var qs = require('querystring')
+	, http = require('http')
+	, npm = require('npm');
+
+// use API to search npm, randomizing the result I get each time. 
+
+var search = process.argv.slice(2).join(' ').trim(); // is pulling the id which is required from the Twitter API
+
+if (!search.length) {
+	return console.log('\n Usage: node tweets <search term>\n'); 
+}
+
+console.log('\n searching for: \033[96m' + search + '\033[39m\n');
+
+http.request({
+		host: 'search.twitter.com'
+	, path: '/search.json?' + qs.stringify({ q:search })
+}, function (res) {
+	var body = '';
+	res.setEncoding('utf8');
+	res.on('data', function (chunk) {
+		body += chunk;
+	});
+	res.on('end', function () {
+		var obj = JSON.parse(body)
+		obj.results.forEach(function (tweet) {
+			console.log(' \033[90m' + tweet.text + '\033[39m');
+			console.log('	\033[94m' + tweet.from_user + '\033[39m');
+			console.log('--');
+		})
+	})
+}).end();
